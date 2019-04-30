db8edff5b647ed3270fb4d47375d70101adb325c
José Claércio <j.claercio8@gmail.com>
2019-04-29 23:34:38 -0300

    finish

--- lab12/lab12/lab12/main.asm
+++ lab12/lab12/lab12/main.asm
@@ -12,7 +12,7 @@ rjmp MAIN
 MAIN:
   ldi R16,0x00
   out DDRC,R16
-  ldi R16,0x1E ; 0001 1110
+  ldi R16,0x0F ; 0001 1110
   out DDRB,R16 
   ldi R16,0b011000000
   sts ADMUX,R16
@@ -27,38 +27,38 @@ MAIN:
   sbrs R15,4
   rjmp WAIT
 
-  lds R16,ADCH
+  lds R18,ADCH
   lds R17,ADCL
 
   THRESHOLDS:
   cpi R17,50
   brlo ONLED1
   cpi R17,100
-  brlo ONLED12
+  breq ONLED12
   cpi R17,150
-  brlo ONLED123
+  breq ONLED123
   cpi R17,230
-  brlo ONLED1234
+  breq ONLED1234
   rjmp READ
   
   ONLED1:
-   ldi R18,0x02  ;0000 0010
-   out PORTB,R18
+   ldi R19,0x01  ;0000 0010
+   out PORTB,R19
   rjmp READ
 
   ONLED12:
-   ldi R18,0x06  ;0000 0110
-   out PORTB,R18
+   ldi R19,0x03  ;0000 0110
+   out PORTB,R19
   rjmp READ
 
    ONLED123:
-   ldi R18,0x0E  ;0000 1110
-   out PORTB,R18
+   ldi R19,0x07  ;0000 1110
+   out PORTB,R19
   rjmp READ
 
    ONLED1234:
-   ldi R18,0x1E  ;0000 0110
-   out PORTB,R18
+   ldi R19,0x0F  ;0001 1110
+   out PORTB,R19
   rjmp READ
      
 
