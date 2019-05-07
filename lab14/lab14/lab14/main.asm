3b8d5fd29ba81279dd52b1eabde8183ee93cb07e
José Claércio <j.claercio8@gmail.com>
2019-05-07 00:02:14 -0300

    aaaa

--- lab14/lab14/lab14/main.asm
+++ lab14/lab14/lab14/main.asm
@@ -1,12 +1,102 @@
 ;
 ; lab14.asm
 ;
-; Created: 02/05/2019 22:31:20
+; Created: 02/05/2019 22:59:15
 ; Author : José
 ;
+.INCLUDE "m328Pdef.inc"
+.org 0x0000 jmp SETUP
+.org 0x002A jmp FUNC
+SETUP: 
+  ldi R16,0x03
+  out DDRC,R16
+  ldi R16,0xFC
+  out DDRD,R16
 
+FUNC:
+ in R17,PINC
+ cpi R17,0x01
+ breq RETAN
+ in R17,PINC
+ cpi R17,0x02
+ breq SERRA
+ in R17,PINC
+ cpi R17,0x03
+ breq TRIAN
+ rjmp FUNC
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+ RETAN:
+  ldi R19,0x01
+  ldi R20,0xFC
+  out PORTB,R19
+  out PORTD,R20
+  rcall delay
+  clr R19
+  clr R20
+  out PORTB,R19
+  out PORTD,R20
+  rcall delay
+  rjmp FUNC
+
+ SERRA:
+  sbi PORTD,2
+  rcall delay
+  sbi PORTD,3
+  rcall delay
+  sbi PORTD,4
+  rcall delay
+  sbi PORTD,5
+  rcall delay
+  sbi PORTD,6
+  rcall delay
+  sbi PORTD,7
+  rcall delay
+  sbi PORTB,0
+  rcall delay
+  clr R17
+  out PORTB,R17
+  out PORTD,R17
+  rjmp FUNC
+
+  TRIAN:
+  sbi PORTD,2
+  rcall delay
+  sbi PORTD,3
+  rcall delay
+  sbi PORTD,4
+  rcall delay
+  sbi PORTD,5
+  rcall delay
+  sbi PORTD,6
+  rcall delay
+  sbi PORTD,7
+  rcall delay
+  cbi PORTD,7
+  rcall delay
+  cbi PORTD,6
+  rcall delay
+  cbi PORTD,5
+  rcall delay
+  cbi PORTD,4
+  rcall delay
+  cbi PORTD,3
+  rcall delay
+  cbi PORTD,2
+  rcall delay
+  rjmp FUNC
+  
+  delay:
+   clr R21
+   clr R22
+   ldi R23,10
+  
+   
+  loop:
+   dec R21
+   brne loop
+   dec R22
+   brne loop
+   dec R23
+   brne loop
+   ret 
+
